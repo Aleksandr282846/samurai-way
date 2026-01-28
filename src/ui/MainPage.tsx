01a504cb47e236db93dbeea4d2ed9ed6cce06346
Aleksandr <aleksandr282846@gmail.com>
2026-01-28 14:27:03 +0200

    change h1 header

--- src/ui/MainPage.tsx
+++ src/ui/MainPage.tsx
@@ -13,7 +13,8 @@ export function MainPage() {
 
   return (
     <div>
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: 'flex', gap: '40px'}}>
+        <h1>MusicFun Player</h1>
         <TracksList
           selectedTrackId={trackId}
           onTrackSelect={handleTrackSelect}
